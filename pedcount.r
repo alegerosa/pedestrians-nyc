5b9d30f2069501fa81752c2741e24d873f8327e2
Ale Gerosa <alegerosa@gmail.com>
2018-10-20 20:32:32 -0400

    Add a map of NYC showing the locations in the dataset

--- pedcount.r
+++ pedcount.r
@@ -1,7 +1,35 @@
 library(tidyverse)
 library(reshape2)
 library(ggmap)
-pedcount <- read.csv("https://raw.githubusercontent.com/alegerosa/pedestrians-nyc/master/PedCountLocationsMay2015.csv")
+register_google(key = Sys.getenv("google_maps_key"))
+pedcount <- read_csv("PedCountLocationsMay2015.csv")
+
+##Let's start with maps
+
+#Add a longitude and a latitud columns
+pedcount2 <- pedcount %>% separate(the_geom, c("temp", "lon", "lat"), sep = " ") %>%
+  select(-temp)
+pedcount2 <- mutate(pedcount2, lon = as.numeric(substr(pedcount2$lon,2,nchar(pedcount2$lon))),
+         lat = as.numeric(substr(pedcount2$lat,1,nchar(pedcount2$lat)-1)))
+
+#Create a map that I'll later overlay the data on
+#Set coordinates for the city
+nyc <- c(lon = -73.9559, lat = 40.7128)
+#Download the map
+nyc_map <- get_map(location = nyc,  zoom = 11)
+ggmap(nyc_map)
+
+#Make a map with a point for each location, color by borough
+ggmap(nyc_map) +
+  geom_point(aes(lon, lat), data = pedcount2)
+
+#And now one colored by bourough and showing by the size of the marks the amount of people counted on May 2015
+ggmap(nyc_map) +
+  geom_point(aes(lon, lat, color = Borough, size = May15_AM, alpha = 0.6), data = pedcount2)
+
+##YAY MAPS!
+
+##Now we keep playing
 
 #Create a more manageable data set to play with, with only the weekday afternoon measurements
 location_count_pm <- select(pedcount,
@@ -13,7 +41,7 @@ location_count_pm <- select(pedcount,
 with_total_difference <- mutate(location_count_pm,
                                  Total_Diff = May15_PM - May07_PM)
 with_total_difference <- arrange(with_total_difference, desc(Total_Diff))
-with_total_difference
+head(with_total_difference)
 
 hist(with_total_difference$Total_Diff)
 
@@ -36,7 +64,5 @@ ggplot(data = df_Manhattan) + geom_line(mapping = aes(x = variable, y = value, g
 ggplot(data = df_Queens) + geom_line(mapping = aes(x = variable, y = value, group = Loc, color = Loc))
 ggplot(data = df_Staten_Island) + geom_line(mapping = aes(x = variable, y = value, group = Loc, color = Loc))
 
-#Create a map that I'll later overlay the data on
-nyc_map <- get_map(location = c(lon = -74.00, lat = 40.71),  zoom = 11)
-ggmap(nyc_map)
+
 
