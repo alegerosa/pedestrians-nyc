76a74d9bb85865075e6e461e657bfb0ae921b338
Ale Gerosa <alegerosa@gmail.com>
2018-11-15 12:56:02 -0500

    Make dataset tidy
    With no values in column names and one row per observation.

--- pedcount.r
+++ pedcount.r
@@ -31,8 +31,9 @@ ggmap(nyc_map) +
 
 ##Now we keep playing
 
+#Playing with it before I learned about tidy data
 #Create a more manageable data set to play with, with only the weekday afternoon measurements
-location_count_pm <- select(pedcount,
+location_count_pm <- select(pedcount2,
                             Borough,
                             Loc,
                             ends_with("PM"))
@@ -64,5 +65,38 @@ ggplot(data = df_Manhattan) + geom_line(mapping = aes(x = variable, y = value, g
 ggplot(data = df_Queens) + geom_line(mapping = aes(x = variable, y = value, group = Loc, color = Loc))
 ggplot(data = df_Staten_Island) + geom_line(mapping = aes(x = variable, y = value, group = Loc, color = Loc))
 
+#This is... cool? But, we had to arbitrarily chose one of the measurements (weekday afternoons) to create a 'more manageable' dataset, because the original dataset is very untidy. So let's make the whole dataset tidy instead.
+
+#We start we we'd left off, at pedcount2. I'll keep making iterative new versions of the data set as we go, to better show the process
+
+head(pedcount2)
+#Many different values are stored as column names
+#Let's start with the columns that store dates as values. These are named with the first letters of the month (May or Sept), an underscore and either D or D2. D is used for weekday measurements and D2 for weekend.
+
+pedcount3 <- pedcount2 %>%
+  gather(key = "date_coding", value = "date", contains("_D")) %>%
+  select(lon, lat, Loc, Borough, Street, From_, To, Index, date_coding, date, everything())
+View(pedcount3)
+
+#I am confused, but I am going to hold that thought and gather the columns with pedestrian counts, to see where it takes me
+
+pedcount4 <- pedcount3 %>%
+  gather(key = "measurement_coding", value = "ped_count", 11:61)
+View(pedcount4)
 
 
+#Now I have a ton of duplicates, because I really need only one combination of Location, date and pedcount, but how do I know which one?
+#If I look at the table, I can identify some that I know are useless/redundant extra rows from seeing contradictions in the date information that was coded in the names and the actual date for that row. So, to get rid of these I need to be able to identify matches and filter by them.
+#For example, I should only keep rows where the the first six characters of date_coding match the first six characters of measurement_coding
+
+pedcount5 <- pedcount4 %>%
+  filter(str_sub(date_coding, 1, 6) == str_sub(measurement_coding, 1, 6))
+View(pedcount5)
+
+#YAY, we got rid of most of them. However, each value is still in two different rows only one of which has properly matching date and measurement coding. We shouldn't have two 'May07_AM' rows for the same location with he same value; the reason we do is that the dates are different, and one is a weekday measurement and the other a weekend measurement. How do we know which is the one to keep? well, Weekdays have one AM and one PM measurement, and weekends have only one measurement, coded MD. So, in this case, we need to keep the row where date_coding is _D. In general, we need to keep all rows where _D in date_coding meets _AM OR _PM in measurement_coding and all rows where _D2 in date_coding matches _MD in measurement coding
+
+pedcount6 <- pedcount5 %>%
+  filter((str_sub(date_coding, -1, -1) == "2" & str_sub(measurement_coding, -1, -1) == "D") | (str_sub(date_coding, -1, -1) == "D" & str_sub(measurement_coding, -1, -1) == "M"))
+View(pedcount6)
+
+#Done! Now we just need to extract from "date_coding" and "measurement_coding" the information that is relevant and not redundant
